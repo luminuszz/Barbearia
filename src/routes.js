8aaff83fd78235918c820fd9a18538017cbc1c2b
Luminuszz <48535259+luminuszz@users.noreply.github.com>
2019-12-18 17:40:31 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -12,34 +12,21 @@ routes.use((req, res, next)=>{
   return next();
 });
 
-const session  =  axios({
+
+
+routes.post(`/new_user_identified.fcgi?session=IWZMTGP4zSmRCJsepA2v4xOu`, (req, res)=>{
+  const decode = qs.decode(req.query);
+  console.log(decode);
+  const session  =  axios({
                 method: 'post',
                 url: "http://192.168.8.2/login.fcgi",
                 data:{
                     login: 'admin',
                     password: 'admin'
                 }
-            });
+            })
 
-
-routes.post(`/`, (req, res)=>{
-  const decode = qs.decode(req.query);
-  console.log(decode);
-  return res.json({
-    'result':
-  {'event': 7,
-    'user_id': 6,
-    'user_name': 'Neal Caffrey',
-    'user_image': false,
-    'portal_id': 1,
-    'actions': [
-      {'action': 'door',
-        'parameters': 'door=1'},
-      {'action': 'door',
-        'parameters': 'door=2'},
-    ],
-  },
-  });
+ 
 });
 
 
